6d6b7123390c8b69414b1307b8176c64be6ee08a
Chantel Lowe <chantellowe17@gmail.com>
2019-09-27 08:00:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     #@side_c > 0 
     
     
-    elsif @side_a == @side_b && @side_b == @side_c
+    if @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
       :isosceles
