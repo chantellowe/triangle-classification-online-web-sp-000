4d85e107877a4c59b8b541576a2bb1aa6cc7fc3a
Chantel Lowe <chantellowe17@gmail.com>
2019-09-27 08:00:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,12 +13,7 @@ class Triangle
     #@side_b > 0 
     #@side_c > 0 
     
-    if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 
-      begin
-        raise TriangleError
-      #rescue TriangleError => error 
-        #puts error.message
-      end
+    
     elsif @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
